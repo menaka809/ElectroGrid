c23ba58458aa1d3c1029b6d1c37c9ce0e6d8bf4d
Methmini-abeysekara <manojaa415@gmail.com>
2022-04-26 23:36:40 +0530

    configure customer controller

--- CustomerInformationManagement/src/main/java/ElectroGrid/CustomerInformationManagement/service/CustomerController.java
+++ CustomerInformationManagement/src/main/java/ElectroGrid/CustomerInformationManagement/service/CustomerController.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 import ElectroGrid.CustomerInformationManagement.constant.Constant;
 import ElectroGrid.CustomerInformationManagement.constant.CustomerConstant;
 import ElectroGrid.CustomerInformationManagement.modal.Customer;
